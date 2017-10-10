dbe87e1085c474e8a0f263df31a1011a4b333193
danjinandjuice <daniel.h.jin@gmail.com>
2017-10-10 10:05:55 -0600

    Initial Commit

---
+++ PokemonHybrid/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PokemonHybrid
+//
+//  Created by Daniel Jin on 10/10/17.
+//  Copyright © 2017 Daniel Jin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
